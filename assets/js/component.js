46a222b9c1a04ae7855d2fd99b5c36d27bd03fc1
ayeong0377 <ayeong0377@gmail.com>
2026-04-06 17:27:21 +0900

    메뉴, kv, info 구현

---
+++ assets/js/component.js
@@ -0,0 +1,21 @@
+$(function(){
+    // menu
+    let menuScroll = 750;
+    $(Window).scroll(function(){
+        let num = $(window).scrollTop();
+
+        if(num > menuScroll){
+            $('.header').css("background-color", "#131313")
+        } else {
+            $('.header').css("background-color", "")
+        }
+    })
+
+    // mo
+    $('.mo-menu-btn').on('click', function(){
+        $('.header__mo-nav').addClass('is-open')
+    });
+    $('.mo-close-btn').on('click', function(){
+        $('.header__mo-nav').removeClass('is-open')
+    });
+})
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+    // info
+    let i = 0
+    setInterval(function(){
+        if(i < 3) {
+            i++
+        } else {
+            i = 0
+        }
+
+        $('.fade-img').fadeOut(300);
+        $('.fade-img').eq(i).fadeIn(300);
+    }, 2800)
+    
+    // tab
     $('.tab.type-tab li').on('click', function(e) {
         e.preventDefault();
 
