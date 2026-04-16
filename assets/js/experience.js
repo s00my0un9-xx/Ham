f500f2115d12bbe622938b139b8a3c0b6d6634e8
kimyunho <kwh3017@gmail.com>
2026-04-16 19:19:35 +0900

    반응형 및 카드 수정 완료했습니다.

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,8 +1,21 @@
 $(function(){
-    // tab switching
-    $('.tab-button').on('click', function(){
+    // PC 탭 (2개 버튼: 패널 2개씩 활성)
+    $('.tab-buttons--pc .tab-button').on('click', function(){
+        var scrollY = window.scrollY;
+        var targets = $(this).data('tabs').split(',');
+        $('.tab-buttons--pc .tab-button').removeClass('is-active');
+        $(this).addClass('is-active');
+        $('.tab-panel').removeClass('is-active');
+        $.each(targets, function(i, id){
+            $('#' + id.trim()).addClass('is-active');
+        });
+        window.scrollTo(0, scrollY);
+    });
+
+    // MO 탭 (4개 버튼: 패널 1개씩 활성)
+    $('.tab-buttons--mo .tab-button').on('click', function(){
         var target = $(this).data('tab');
-        $('.tab-button').removeClass('is-active');
+        $('.tab-buttons--mo .tab-button').removeClass('is-active');
         $(this).addClass('is-active');
         $('.tab-panel').removeClass('is-active');
         $('#' + target).addClass('is-active');
