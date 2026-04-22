98a1ed96bef9de6da346784b705577a16dfd3d6e
soomyoung <s00my0un9@gmail.com>
2026-04-22 15:41:20 +0900

    막걸리 서브페이지 js 주석 삭제

--- assets/js/MAKGEOLLI.js
+++ assets/js/MAKGEOLLI.js
@@ -19,10 +19,8 @@ $(function () {
     $(".faqmak__question").click(function () {
         var $item = $(this).closest(".faqmak__item");
 
-        // 다른 항목 닫기 (필요하다면)
         $(".faqmak__item").not($item).removeClass("active").find(".faqmak__answer").slideUp();
 
-        // 현재 항목 토글
         $item.toggleClass("active");
         $item.find(".faqmak__answer").stop().slideToggle(300);
     });
